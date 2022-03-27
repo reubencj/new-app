4b67fa2397d4a8c481a0c5826fe36516f81bce0f
vojewole <vojewole@gmail.com>
2022-03-26 17:05:37 -0700

    made updates to navbar, feedpage, feedcard and favorite card

--- client/src/components/smallComponents/FavoritesCard.js
+++ client/src/components/smallComponents/FavoritesCard.js
@@ -1,8 +1,26 @@
 const FavoritesCard = (props) => {
+
+    const [news, setNews] = useState([]); //The state that we could set when we pull from the Api
     
     return (
         <div>
-
+            <div>
+                <Navbar userId={userId}/>
+            </div>
+            <div>
+                <h1>Hi, {userName} here are your favorites</h1>
+            </div>
+            <div  className="container border border-dark">
+                <div>
+                    <h1>{news.title}</h1> 
+                </div>
+                <div>
+                    <img src={news.image} alt=""></img>
+                </div>
+                <div>
+                <p>{news.description}</p>
+                </div>
+            </div>
         </div>
     )
 }
--- client/src/components/smallComponents/FeedCard.js
+++ client/src/components/smallComponents/FeedCard.js
@@ -1,8 +1,30 @@
+import { useState } from "react";
+
 const FeedCard = (props) => {
     
-    return (
-        <div>
+    const [news, setNews] = useState([]); //The state that we could set when we pull from the Api
+    const [favorite, setFavorite] = useState(false);
 
+    const setButtonState = () => { //To make the button toggle from filled to filled star if favorited or unfilled star
+                                   //We can add the addtional code to this function
+        setFavorite(!favorite);
+    }
+
+    return (
+        <div  className="container border border-dark">
+            <div>
+                <h1>{news.title}</h1> 
+            </div>
+            <div>
+                <img src={news.image} alt=""></img>
+            </div>
+            <div>
+                {favorite?
+                <button type="button" className="btn btn-warning"><span className="bi bi-star-fill" onClick={(e) => {setButtonState()}}></span>Favorite</button>
+                :
+                <button type="button" className="btn btn-warning"><span className="bi bi-star" onClick={(e) => {setButtonState()}}></span>Favorite</button>
+                }
+            </div>
         </div>
     )
 }
