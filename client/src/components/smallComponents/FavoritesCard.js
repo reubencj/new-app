f4a2e44853ee741025a6a67e4848570112bd3dfe
Victor Ojewole <70405911+vojewole@users.noreply.github.com>
2022-03-30 17:47:08 -0700

    Add files via upload

--- client/src/components/smallComponents/FavoritesCard.js
+++ client/src/components/smallComponents/FavoritesCard.js
@@ -4,17 +4,31 @@ const FavoritesCard = (props) => {
     //const [news, setNews] = useState([]); //The state that we could set when we pull from the Api
     
     return (
-            <div  className="container border border-dark">
+        <div class="card">
+            <div class="card-header">
+                <h1>{article.title}</h1> 
+            </div>
+            <div class="card-body">
+                <img src={article.media} alt=""></img>
+            </div>
+            <div class="card-footer text-muted">
+                <p>{article.summary}</p>
                 <div>
-                    <h1>{article.title}</h1> 
-                </div>
-                <div>
-                    <img src={article.media} alt=""></img>
-                </div>
-                <div>
-                    <p>{article.summary}</p>
+                    <button className="btn btn-info"><Link to={{pathname:"/details", state:article}}>View Page</Link></button>
                 </div>
             </div>
+        </div>
+            // <div  className="container border border-dark">
+            //     <div>
+            //         <h1>{article.title}</h1> 
+            //     </div>
+            //     <div>
+            //         <img src={article.media} alt=""></img>
+            //     </div>
+            //     <div>
+            //         <p>{article.summary}</p>
+            //     </div>
+            // </div>
     )
 }
 
--- client/src/components/smallComponents/FeedCard.js
+++ client/src/components/smallComponents/FeedCard.js
@@ -5,7 +5,7 @@ import axios from "axios";
 const FeedCard = (props) => {
     const {article} = props;
     //const [news, setNews] = useState([]); //The state that we could set when we pull from the Api
-    const [favorite, setFavorite] = useState(false);
+    const [favorite, setFavorite] = useState(true);
     const [title, setTitle] = useState(article.author);
     const [author, setAuthor] = useState(article.author);
     const [published_date, setPublished_date] = useState(article.published_date);
@@ -41,47 +41,45 @@ const FeedCard = (props) => {
         setFavorite(!favorite);
     }
 
-    const removeFromFavoriteHandler = (e) => { //Can we do soemthing like this if someone decides they don't actually want to favorite an item???
-        e.preventDefault();
-        axios.delete('http://localhost:8000/api/favorites',
-        {
-        title,
-        author,
-        published_date,
-        link,
-        topic,
-        source,
-        media,
-        excerpt,
-        summary,
-        _id
-        }
-        )
-        .then(res=> console.log(res))
-        .catch(err => console.log(err))
-        setFavorite(!favorite);
-        }
-
-
     return (
-        <div  className="container border border-dark">
-            <div>
+        <div className="card">
+            <div class="card-header">
                 <h1>{article.title}</h1> 
             </div>
-            <div>
+            <div class="card-body">
                 <img src={article.media} alt=""></img>
             </div>
-            <div>
-                {favorite?
-                <button type="button" className="btn btn-warning"><span className="bi bi-star-fill" onClick={(e) => {addToFavoriteHandler()}}></span>Favorite</button>
-                :
-                <button type="button" className="btn btn-warning"><span className="bi bi-star" onClick={(e) => {removeFromFavoriteHandler()}}></span>Favorite</button>
-                }
-            </div>
-            <div>
-                <button className="btn btn-info"><Link to={{pathname:"/details", state:article}}>View Page</Link></button>
+            <div class="card-footer text-muted">
+                <div>
+                    {favorite?
+                        <button type="button" className="btn btn-warning"><span className="bi bi-star" onClick={(e) => {addToFavoriteHandler()}}></span>Favorite</button>
+                        :
+                        <button type="button" disabled={true} className="btn btn-warning"><span className="bi bi-star-fill" ></span>Favorite</button>
+                    }
+                </div>
+                <div>
+                    <button className="btn btn-info"><Link to={{pathname:"/details", state:article}}>View Page</Link></button>
+                </div>
             </div>
         </div>
+        // <div  className="container border border-dark">
+        //     <div>
+        //         <h1>{article.title}</h1> 
+        //     </div>
+        //     <div>
+        //         <img src={article.media} alt=""></img>
+        //     </div>
+        //     <div>
+        //         {favorite?
+        //         <button type="button" className="btn btn-warning"><span className="bi bi-star-fill" onClick={(e) => {addToFavoriteHandler()}}></span>Favorite</button>
+        //         :
+        //         <button type="button" className="btn btn-warning"><span className="bi bi-star" onClick={(e) => {removeFromFavoriteHandler()}}></span>Favorite</button>
+        //         }
+        //     </div>
+        //     <div>
+        //         <button className="btn btn-info"><Link to={{pathname:"/details", state:article}}>View Page</Link></button>
+        //     </div>
+        // </div>
     )
 }
 
