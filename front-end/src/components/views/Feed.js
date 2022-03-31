dc29d5503c1d377dffc850be884cf5c9af582dc6
Reuben John <reuben.chacko.john@gmail.com>
2022-03-31 13:51:47 -0700

    feed completed

--- front-end/src/components/views/Feed.js
+++ front-end/src/components/views/Feed.js
@@ -17,26 +17,51 @@ const Feed = (props) => {
   const [loaded, setLoaded] = useState(false);
 
   useEffect(() => {
-    axios.get("http://localhost:8000/api/feed/", CONFIG).then((res) => {
-      setUserInterest(res.data.message.user_interests);
-      console.dir(res.data.message);
-      setSelectedInterest(res.data.message.select_interest);
-      setArticles(res.data.message.articles);
-      setPage(res.data.message.page);
-      setTotalPage(res.data.message.total_pages);
-      setLoaded(true);
-    });
+    axios
+      .get("http://localhost:8000/api/feed/", CONFIG)
+      .then((res) => {
+        setUserInterest(res.data.message.user_interests);
+        console.dir(res.data.message);
+        setSelectedInterest(res.data.message.select_interest);
+        setArticles(res.data.message.articles);
+        setTotalPage(res.data.message.total_pages);
+        setLoaded(true);
+      })
+      .catch((err) => console.log(err));
   }, []);
+
+  const getArticle = () => {
+    axios
+      .get(`http://localhost:8000/api/feed/${selectedInterest}/${page}`, CONFIG)
+      .then((res) => {
+        setUserInterest(res.data.message.user_interests);
+        console.dir(res.data.message);
+        setSelectedInterest(res.data.message.select_interest);
+        setArticles(res.data.message.articles);
+        setPage(res.data.message.page);
+        setTotalPage(res.data.message.total_pages);
+      });
+  };
 
   const nextPage = (e) => {
     e.preventDefault();
-    setPage(page + 1);
+    if (page < totalPage) {
+      setPage(page + 1);
+    }
   };
 
   const prevPage = (e) => {
     e.preventDefault();
-    setPage(page - 1);
+    if (page > 1) {
+      setPage(page - 1);
+    }
   };
+
+  useEffect(() => {
+    if (loaded) {
+      getArticle();
+    }
+  }, [page, selectedInterest]);
 
   return (
     <div>
@@ -44,37 +69,53 @@ const Feed = (props) => {
         <Navbar />
       </div>
       <div>{/* <h1>Hi, {userID} welcome to your feed</h1> */}</div>
-      <div className="container">
-        <div className="row mt-3">
-          <div className="col-sm-4">
-            <select
-              className="form-select"
-              aria-label="multiple select example"
-              onChange={(e) => setSelectedInterest(e.target.value)}
-              value={selectedInterest}
-            >
-              {userInterest.map((interest, index) => {
-                return (
-                  <option key={index} value={interest}>
-                    {interest}
-                  </option>
-                );
-              })}
-            </select>
+      {loaded && (
+        <div className="container">
+          <div className="row mt-3">
+            <div className="col-md-5">
+              <select
+                className="form-select"
+                aria-label="multiple select example"
+                onChange={(e) => {
+                  setSelectedInterest(e.target.value);
+                  setPage(1);
+                }}
+                value={selectedInterest}
+              >
+                {userInterest.map((interest, index) => {
+                  return (
+                    <option key={index} value={interest}>
+                      {interest}
+                    </option>
+                  );
+                })}
+              </select>
+            </div>
           </div>
-
-          <div className="col-sm-4">
-            <button className="btn btn-primary">Get News</button>
+          <div className="row mt-3">
+            {articles.map((article) => {
+              return (
+                <div className="col-md-4 mt-4" key={article._id}>
+                  <FeedCard data={article} />
+                </div>
+              );
+            })}
           </div>
         </div>
-        <div className="row mt-3">
-          {articles.map((article) => {
-            return (
-              <div className="col-md-4 mt-4" key={article._id}>
-                <FeedCard data={article} />
-              </div>
-            );
-          })}
+      )}
+      <div className="row mb-4">
+        <div className="col-4 ">
+          <button className="btn btn-primary" onClick={(e) => prevPage(e)}>
+            Previous Page
+          </button>
+        </div>
+        <div className="col-4">
+          <p className="h6">Page {page}</p>
+        </div>
+        <div className="col-4">
+          <button className="btn btn-primary" onClick={(e) => nextPage(e)}>
+            Next Page
+          </button>
         </div>
       </div>
     </div>
